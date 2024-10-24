fe0d7414a06224df8758f1fd9b0a94cc72e6c5f1
Flem0 <24416002@studentmail.ul.ie>
2024-10-24 15:52:55 +0100

    Layout for calculation of ratios between cycles.

---
+++ CalculationsLayout.java
@@ -0,0 +1,36 @@
+//importing class libraries to use Json files
+import com.fasterxml.Jackson.databind.JsonNode;
+import com.fasterxml.Jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class CalculationsLayout{
+    public static void main(String[] args) {
+        //Parse json files
+
+        //Load cycle one data
+
+        //Load cycle two data
+
+        //json structure
+
+        //Set up arrays
+
+        //Set up list to store ratio
+
+        //Calculate each ratio
+
+        //Error prevention (dividing by zero etc)
+
+
+    }
+}
+
+
+
+
+
+
+public class CalculationsLayout {
+}
